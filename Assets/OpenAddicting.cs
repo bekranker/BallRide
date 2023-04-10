ad9c50b94fe19ce90f8a52a2b2200dc8c161cedd
Bekir <bekirekiz@ogrenci.beykoz.edu.tr>
2023-04-10 21:48:47 +0300

    assa

---
+++ Assets/OpenAddicting.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenAddicting : MonoBehaviour
+{
+
+    public void OpenURL()
+    {
+        Application.OpenURL("https://www.addictinggames.com/");
+    }
+
+}
